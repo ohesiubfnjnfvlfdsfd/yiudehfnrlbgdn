11856abe98a6b042ebb22380f868898f4def9f15
ohesiubfnjnfvlfdsfd <139222219+ohesiubfnjnfvlfdsfd@users.noreply.github.com>
2023-07-12 08:02:58 +0100

    Add files via upload

---
+++ CHAMELEONone/jg.js
@@ -0,0 +1,28 @@
+// prevent ctrl + s
+$(document).bind('keydown', function(e) {
+if(e.ctrlKey && (e.which == 83)) {
+e.preventDefault();
+return false;
+}
+});
+
+document.addEventListener('contextmenu', event => event.preventDefault());
+
+document.onkeydown = function(e) {
+if (e.ctrlKey && 
+(e.keyCode === 67 ||   
+e.keyCode === 86 || 
+e.keyCode === 85 || 
+e.keyCode === 117)) {
+return false;
+} else {
+return true;
+}
+};
+$(document).keypress("u",function(e) {
+if(e.ctrlKey)
+{
+return false;      }
+else {
+return true;
+}});
